aef83ce7895d3db9e3cc7f7d66c8870eab892697
chun7953 <isuke118@gmail.com>
2026-09-09 13:53:41 +0900

    Keep calendar core out of deferred bundle and fold settings

--- public/common/admin-future-scope.js
+++ public/common/admin-future-scope.js
@@ -1,5 +1,5 @@
-import './admin-calendar-month-view.js';
 import './admin-calendar-settings.js';
+import './admin-calendar-settings-fold.js';
 import './admin-mentions.js';
 import './admin-preview.js';
 import './admin-calendar-edit.js';
